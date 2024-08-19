ea6f76870c816ee43f48916c7e2d19f904b1bc8c
ersuamine <amine.ersu@gmail.com>
2024-08-19 02:34:28 -0400

    project setup

---
+++ src/test/java/net/trycloud/step_definitions/Hooks.java
@@ -0,0 +1,52 @@
+package net.trycloud.step_definitions;
+
+
+
+
+import net.trycloud.utilities.BrowserUtils;
+import net.trycloud.utilities.ConfigurationReader;
+import net.trycloud.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.time.Duration;
+
+/*
+In this class we will be able to create "pre" and "post" condition
+for ALL the SCENARIOS and even STEPS.
+ */
+public class Hooks {
+
+    //import the @Before coming from io.cucumber.java
+    @Before (order = 1)
+    public void setupMethod(){
+
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+    }
+
+    /*
+    @After will be executed automatically after EVERY scenario in the project.
+     */
+    @After
+    public void teardownMethod(Scenario scenario){
+
+        if (scenario.isFailed()) {
+
+            byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+
+        }
+
+        BrowserUtils.sleep(2);
+        Driver.closeDriver();
+
+    }
+
+
+
+}
---
+++ src/test/resources/features/login.feature
@@ -0,0 +1,8 @@
+@login
+Feature: As a user, I should be able to log in.
+
+
+
+
+
+
